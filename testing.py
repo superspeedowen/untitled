0de203e6e59ffaa2a21f6d96c9b7c69a673e19e9
Allen U <uallen@macau-airport.com>
2016-06-07 16:12:57 +0800

    Initial commit

---
+++ testing.py
@@ -0,0 +1,10 @@
+class testing:
+    def __init__(self):
+        print("hello1111")
+        self.i = 9522
+
+    def hello(self):
+        print("hello", self.i)
+
+#a = Demo()
+#a.hello()
---
+++ testing11.py
@@ -0,0 +1,11 @@
+class testing:
+    def __init__(self):
+        print("hello11")
+        self.i = 9527
+
+    def hello(self):
+        print("hello", self.i)
+        print("123")
+
+#a = Demo()
+#a.hello()
